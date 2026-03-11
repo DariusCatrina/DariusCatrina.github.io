8bc06ce17d124cbbd6ae9d6df0a322e480098cab
DariusCatrina <44647822+DariusCatrina@users.noreply.github.com>
2026-03-11 02:28:12 +0000

    Deploying to gh-pages from @ DariusCatrina/DariusCatrina.github.io@8a37e17ffb630cd4023bb8918bf402f3512cda95 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "DariusCatrina/dgcportofolio",
+    "data-repo": "DariusCatrina/DariusCatrina.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
